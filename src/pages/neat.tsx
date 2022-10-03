3a4749295101a6f56815bf64f91da71ceeae346c
Blake Barnhill <blake.barnhill@gmail.com>
2022-10-03 14:26:45 -0400

    Fixed build errors

--- src/pages/neat.tsx
+++ src/pages/neat.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import Header from "../components/header";
 import { Prism } from '@mantine/prism';
 
