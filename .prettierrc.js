828dd800f1c1c5eedc9dc48b3cc98573bc30a524
Blake Barnhill <blakeb21@vt.edu>
2024-09-27 15:28:11 -0400

    Add Posthog

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,31 @@
 // src/pages/_app.tsx
-import { withTRPC } from "@trpc/next";
-import type { AppRouter } from "../server/router";
-import type { AppType } from "next/dist/shared/lib/utils";
-import { Analytics } from '@vercel/analytics/react'
-import superjson from "superjson";
-import "../styles/globals.css";
+import type { AppType } from 'next/dist/shared/lib/utils';
+import '../styles/globals.css';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-    <Component {...pageProps} />
-    <Analytics /></>
-  )
-    
-  
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
+    </>
+  );
 };
 
-export default MyApp
+export default MyApp;
 // const getBaseUrl = () => {
 //   if (typeof window !== "undefined") return ""; // browser should use relative url
 //   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
